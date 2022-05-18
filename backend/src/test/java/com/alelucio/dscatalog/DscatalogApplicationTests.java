bae3985fe89323ba6572389edc854e30b5555ee2
Alessandro Lucio <alessandroluciotr@gmail.com>
2022-05-18 10:39:44 -0300

    h2 database rodando

--- backend/src/test/java/com/alelucio/dscatalog/DscatalogApplicationTests.java
+++ backend/src/test/java/com/alelucio/dscatalog/DscatalogApplicationTests.java
@@ -1,4 +1,4 @@
-package com.alelucio.dscatalog;
+/*package com.alelucio.dscatalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class DscatalogApplicationTests {
 	}
 
 }
+*/
